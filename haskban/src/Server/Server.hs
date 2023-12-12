5aa59eac347128e45bcf25a6a9571ca1644cd935
Viraj Shah <viraj0605@gmail.com>
2023-12-11 21:32:32 -0800

    server done. GET and POST.

--- haskban/src/Server/Server.hs
+++ haskban/src/Server/Server.hs
@@ -7,16 +7,16 @@ module Server.Server where
 import           Network.Wai
 import           Network.HTTP.Types
 import           Network.Wai.Handler.Warp (run)
-import           Control.Concurrent.STM (atomically, newTVarIO, readTVar, modifyTVar)
-import           Control.Monad.IO.Class (liftIO)
-import           Data.Aeson (encode)
-import           Data.IORef
+import           Control.Concurrent.STM (TVar, newTVarIO, readTVarIO, readTVar, writeTVar, atomically)
+import           System.IO.Unsafe (unsafePerformIO)
+import           Data.Aeson (encode, decode)
 import           Schema
+import           Data.Time (UTCTime)
 import qualified Text.Read as T
 import qualified Data.Map as M
 import qualified Data.List as L
 import qualified Data.ByteString.Char8 as B
-import Control.Exception (try)
+import qualified Data.ByteString.Lazy as BL
 
 
 task1 :: TaskBody
@@ -47,8 +47,8 @@ task2 = Task {
     taskLastUpdatedById = 1
 }
 
-allTasks :: Task
-allTasks = M.fromList [(0, task1), (1, task2)]
+initTasks :: Task
+initTasks = M.fromList [(0, task1), (1, task2)]
 
 allProfiles :: Profile
 allProfiles = M.fromList [(0, "Viraj"), (1, "Someone")]
@@ -58,7 +58,6 @@ readIntOnly bs = case T.readMaybe (B.unpack bs) :: Maybe Int of
     Nothing -> Nothing
     Just i -> Just i
 
--- TODO: error handling when v is not an int
 parseQueryItem :: QueryItem -> Maybe Int
 parseQueryItem (k, v) = case k of
     "id" -> case v of
@@ -66,37 +65,51 @@ parseQueryItem (k, v) = case k of
         Just bs -> readIntOnly bs
     _ -> Nothing
 
+getTask :: TVar Task -> Maybe Int -> [TaskBody]
+getTask _ Nothing = []
+getTask allTasks (Just tid) = do 
+    let tasks = unsafePerformIO (readTVarIO allTasks) 
+    case (tid, M.lookup tid tasks) of
+        (_, Just task_body) -> [task_body]
+        (-1, Nothing) -> M.elems tasks
+        _ -> []
 
-getTask :: Maybe Int -> [TaskBody]
-getTask Nothing = M.elems allTasks
-getTask (Just id) = case (id, M.lookup id allTasks) of
-    (id, Just task) -> [task]
-    (-1, Nothing) -> M.elems allTasks
-    _ -> []
-
-handleGETrequest :: Request -> Response
-handleGETrequest request = do
+handleGETrequest :: TVar Task -> Request -> Response
+handleGETrequest allTasks request = do
     let queries = queryString request
     let tid = parseQueryItem (L.head queries)
-    let queriedTask = getTask tid
+    let queriedTask = getTask allTasks tid
     if null queriedTask then
-        responseLBS status404 [("Content-Type", "application/json")] (encode queriedTask)
+        responseLBS status404 [("Content-Type", "text/plain")] "Task not found" 
     else
         responseLBS status200 [("Content-Type", "application/json")] (encode queriedTask)
 
--- handlePOSTrequest = error "Not Implemented"
+createTask :: TVar Task -> TaskBody -> IO ()
+createTask allTasks newTask = do
+    atomically writeTask
+    where
+        writeTask = do
+            tasks <- readTVar allTasks
+            let allkeys = M.keys tasks
+            let newId = if null allkeys then 0 else maximum allkeys + 1
+            let updatedTasks = M.insert newId newTask tasks
+            (writeTVar allTasks updatedTasks)
 
-app :: Application
-app request respond = case requestMethod request of
-    "GET" -> respond (handleGETrequest request)
+app :: TVar Task -> Application
+app allTasks request respond = case requestMethod request of
+    "GET" -> respond (handleGETrequest allTasks request)
     "POST" -> do
-        respond (responseLBS status200 [("Content-Type", "text/plain")] "test Post")
+        body <- strictRequestBody request
+        case decode body of
+            Just newTask -> (createTask allTasks newTask) >> (putStrLn $ "Received new task: " ++ show (newTask :: TaskBody)) >> respond (responseLBS status200 [("Content-Type", "text/plain")] "Task created successfully")
+            Nothing -> putStrLn "Invalid request body" >> respond (responseLBS status400 [("Content-Type", "text/plain")] "Invalid request body")
     _ -> do
         respond (responseLBS status405 [("Content-Type", "test/plain")] "Method Not Allowed")
+
 
 -- TODO: what to do if 8080 is already in use?
 serverMain :: IO ()
 serverMain = do
-    taskBoard <- newTVarIO [] -- Initialize an empty task board
+    allTasks <- newTVarIO initTasks -- Initialize an empty task board
     putStrLn "Starting server on port 8080 (http://localhost:8080). Press Ctrl-C to quit."
-    run 8080 app
+    run 8080 (app allTasks)

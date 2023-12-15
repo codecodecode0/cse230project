38dfa75f82428c36c8ae2b629d5d9db4cb64dd77
Viraj Shah <viraj0605@gmail.com>
2023-12-14 23:52:59 -0800

    added unit test

--- haskban/test/Spec.hs
+++ haskban/test/Spec.hs
@@ -1,2 +1,29 @@
+import Draw
+
+prop_isSelected :: [Int] -> String -> Int -> Bool -> Bool
+prop_isSelected pt colTitle idx expected = (isSelected pt colTitle idx) == expected
+
+isSelected_test_cases :: [([Int], String, Int, Bool)]
+isSelected_test_cases = 
+    [
+        ([0, 1], "To Do", 1, True),
+        ([0, 2], "To Do", 3, False),
+        ([2, 1], "In Progress", 1, False),
+        ([2, 0], "Done", 0, True)
+    ]
+
+
+
+passedCount :: [Bool]->Int 
+passedCount [] = 0
+passedCount (x:xs) = 
+    if x == True 
+    then 1 + passedCount(xs) 
+    else passedCount(xs)
+
+testingResults :: [([Int], String, Int, Bool)] -> [Bool]
+testingResults = map $ (\(x, y, z,w) -> (prop_isSelected x y z w))  
+    
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = putStrLn ("Passed " ++ show (passedCount (testingResults isSelected_test_cases)) ++ " out of " ++ show (length isSelected_test_cases) ++ " test cases in total.")
+-- main = putStrLn ("Hi there")

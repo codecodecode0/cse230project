f28dee2cdfc9d739b9dd7b428639650aa22c9d80
Shril Mody <shmody@ucsd.edu>
2023-12-15 11:51:55 -0800

    small changes

--- haskban/src/Form.hs
+++ haskban/src/Form.hs
@@ -39,28 +39,7 @@ mkForm =
       --     Just d -> d > read "2019-01-01 00:00:00 UTC"
 
 
--- mkForm :: TaskFormData -> Form TaskFormData e ResourceName
--- mkForm = 
---   newForm [
---     (str "Name: " <+>) @@= 
---       editTextField nameForm FormTitle (Just 1),
---     (str "Description: " <+>) @@=
---       editTextField descForm FormDescription (Just 1),
---     (str "Priority: " <+>) @@=
---       radioField taskPriorityForm [ (Low,FormPriority, "Low")
---                               , (Medium,FormPriority, "Medium")
---                               , (High,FormPriority, "High")
---                               ],
---     (str "Status: " <+>) @@=
---       radioField statusForm [ (Todo,FormStatus, "Todo")
---                         , (InProgress,FormStatus, "InProgress")
---                         , (Completed,FormStatus, "Completed")
---                         ],
---     (str "Assigned To: " <+>) @@=
---       editTextField assignedToIdForm FormAssignedToId (Just 1)
---   ]
-
 mkFilterForm :: FilterFormData -> Form FilterFormData e ResourceName
 mkFilterForm = 
-  let label s w = padBottom (Pad 1) $ (vLimit 1 $ hLimit 15 $ str s <+> fill ' ') <+> w
+  let label s w = padBottom (Pad 1) $ (vLimit 1 $ hLimit 30 $ str s <+> fill ' ') <+> w
     in newForm [ label "FilterTasksby Assigned to:" @@= editTextField filterAssignedToIdForm FormAssignedToId (Just 1)]

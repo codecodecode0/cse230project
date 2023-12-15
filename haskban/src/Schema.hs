0b9960c396de0641c902b09e2017978e1c6583a8
Viraj Shah <viraj0605@gmail.com>
2023-12-14 21:25:18 -0800

    added DELETE method and taskID in request

--- haskban/src/Schema.hs
+++ haskban/src/Schema.hs
@@ -8,23 +8,26 @@ import GHC.Generics (Generic)
 import Data.Aeson
 import Data.Map (Map)
 
-data TaskStatus = Backlog | Todo | InProgress | Completed deriving (Show, Eq, Generic, ToJSON, FromJSON)
+data TaskStatus = Todo | InProgress | Completed deriving (Show, Eq, Generic, ToJSON, FromJSON)
 
 data TaskPriority = Low | Medium | High deriving (Show, Eq, Generic, ToJSON, FromJSON)
 
 
+data ReceiveBody = Receive {
+    _taskTitle :: String,
+    _taskDescription :: Maybe String,
+    _taskOwnerId :: String,
+    _taskStatus :: TaskStatus,
+    _taskDueDate :: Maybe UTCTime,
+    _taskAssignedToId :: Maybe String, 
+    _taskPriority :: TaskPriority
+} deriving (Show, Eq, Generic, ToJSON, FromJSON)
+
 data TaskBody = Task {
-    taskTitle :: String,
-    taskDescription :: Maybe String,
-    taskOwnerId :: Int,
-    taskStatus :: TaskStatus,
-    taskDueDate :: Maybe UTCTime,
-    taskAssignedToId :: Maybe Int, 
-    taskPriority :: Maybe TaskPriority,
-    taskCreatedAt :: UTCTime,
-    taskUpdatedAt :: UTCTime,
-    taskLastUpdatedById :: Int
+    _taskId :: Maybe Int,
+    _receievedBody :: ReceiveBody,
+    _taskCreatedAt :: UTCTime,
+    _taskUpdatedAt :: UTCTime
 } deriving (Show, Eq, Generic, ToJSON, FromJSON)
 
 type Task = Map Int TaskBody
-type Profile = Map Int String
--- haskban/src/Server/Server.hs
+++ haskban/src/Server/Server.hs
@@ -11,47 +11,81 @@ import           Control.Concurrent.STM (TVar, newTVarIO, readTVarIO, readTVar, 
 import           System.IO.Unsafe (unsafePerformIO)
 import           Data.Aeson (encode, decode)
 import           Schema
-import           Data.Time (UTCTime)
+import           Data.Time (getCurrentTime)
 import qualified Text.Read as T
 import qualified Data.Map as M
 import qualified Data.List as L
 import qualified Data.ByteString.Char8 as B
-import qualified Data.ByteString.Lazy as BL
 
 
 task1 :: TaskBody
 task1 = Task {
-    taskTitle = "Task 1",
-    taskDescription = Just "Some Description",
-    taskOwnerId = 1,
-    taskStatus = Todo,
-    taskDueDate = Nothing,
-    taskAssignedToId = Nothing,
-    taskPriority = Nothing,
-    taskCreatedAt = read "2023-12-12 00:00:00",
-    taskUpdatedAt = read "2023-12-12 00:00:00",
-    taskLastUpdatedById = 1
+    _taskId = Just 0,
+    _taskCreatedAt = read "2023-12-12 00:00:00",
+    _taskUpdatedAt = read "2023-12-12 00:00:00",
+    _receievedBody = Receive {
+        _taskTitle = "Task 1",
+        _taskDescription = Just "Some Description",
+        _taskOwnerId = "Viraj",
+        _taskStatus = Todo,
+        _taskDueDate = Nothing,
+        _taskAssignedToId = Nothing,
+        _taskPriority = Low
+    }
 }
 
 task2 :: TaskBody
 task2 = Task {
-    taskTitle = "Task 2",
-    taskDescription = Just "Some Description",
-    taskOwnerId = 1,
-    taskStatus = Todo,
-    taskDueDate = Nothing,
-    taskAssignedToId = Nothing,
-    taskPriority = Nothing,
-    taskCreatedAt = read "2023-12-12 00:00:00",
-    taskUpdatedAt = read "2023-12-12 00:00:00",
-    taskLastUpdatedById = 1
+    _taskId = Just 1,
+    _taskCreatedAt = read "2023-12-12 00:00:00",
+    _taskUpdatedAt = read "2023-12-12 00:00:00",
+    _receievedBody = Receive {
+        _taskTitle = "Task 2",
+        _taskDescription = Just "Some Description",
+        _taskOwnerId = "Viraj",
+        _taskStatus = Todo,
+        _taskDueDate = Nothing,
+        _taskAssignedToId = Nothing,
+        _taskPriority = High
+    }
 }
 
-initTasks :: Task
-initTasks = M.fromList [(0, task1), (1, task2)]
+task3 :: TaskBody
+task3 = Task {
+    _taskId = Just 2,
+    _taskCreatedAt = read "2023-12-12 00:00:00",
+    _taskUpdatedAt = read "2023-12-12 00:00:00",
+    _receievedBody = Receive {
+        _taskTitle = "Task 2",
+        _taskDescription = Just "This is an in progress task.",
+        _taskOwnerId = "Viraj",
+        _taskStatus = InProgress,
+        _taskDueDate = Nothing,
+        _taskAssignedToId = Nothing,
+        _taskPriority = Low
+    }
+}
 
-allProfiles :: Profile
-allProfiles = M.fromList [(0, "Viraj"), (1, "Someone")]
+task4 :: TaskBody
+task4 = Task {
+    _taskId = Just 3,
+    _taskCreatedAt = read "2023-12-12 00:00:00",
+    _taskUpdatedAt = read "2023-12-12 00:00:00",
+    _receievedBody = Receive {
+        _taskTitle = "Completed Task",
+        _taskDescription = Just "This task is completed. Yay!",
+        _taskOwnerId = "Viraj",
+        _taskStatus = Completed,
+        _taskDueDate = Nothing,
+        _taskAssignedToId = Nothing,
+        _taskPriority = Low
+    }
+}
+
+
+
+initTasks :: Task
+initTasks = M.fromList [(0, task1), (1, task2), (2, task3), (3, task4)]
 
 readIntOnly :: B.ByteString -> Maybe Int
 readIntOnly bs = case T.readMaybe (B.unpack bs) :: Maybe Int of
@@ -84,7 +118,7 @@ handleGETrequest allTasks request = do
     else
         responseLBS status200 [("Content-Type", "application/json")] (encode queriedTask)
 
-createTask :: TVar Task -> TaskBody -> IO ()
+createTask :: TVar Task -> ReceiveBody -> IO ()
 createTask allTasks newTask = do
     atomically writeTask
     where
@@ -92,8 +126,54 @@ createTask allTasks newTask = do
             tasks <- readTVar allTasks
             let allkeys = M.keys tasks
             let newId = if null allkeys then 0 else maximum allkeys + 1
-            let updatedTasks = M.insert newId newTask tasks
+            let toAddTask = Task {
+                _taskId = Just newId,
+                _receievedBody = newTask,
+                _taskCreatedAt = current_time,
+                _taskUpdatedAt = current_time
+            }
+            let updatedTasks = M.insert newId toAddTask tasks
             (writeTVar allTasks updatedTasks)
+        current_time = unsafePerformIO getCurrentTime
+
+
+-- updateTask :: TVar Task -> TaskBody -> IO ()
+-- updateTask allTasks currTask = do
+--     atomically writeTask
+--     where
+--         writeTask = do
+--             tasks <- readTVar allTasks
+--             let currId = taskId currTask
+--             case (currId, M.lookup tasks currId) of
+--                 (Nothing, _) -> return ()
+--                 (_, Nothing) -> return ()
+--                 (Just tid, Just prevTask) -> do
+--                     let toAddTask = Task {
+--                         taskId = tid,
+--                         receievedBody = currTask,
+--                         taskCreatedAt = prevTask.taskCreatedAt,
+--                         taskUpdatedAt = getCurrentTime
+--                     }
+--                     let updatedTasks = M.insert tid toAddTask tasks
+--                     (writeTVar allTasks updatedTasks)
+
+-- handleDELETErequest :: TVar Task -> Request -> Response
+-- handleDELETErequest allTasks request = do
+    
+
+
+deleteTask :: TVar Task -> Maybe Int -> IO ()
+deleteTask _ Nothing = putStrLn "Invalid task id"
+deleteTask allTasks (Just tid) = do
+    atomically writeTask
+    where
+        writeTask = do
+            tasks <- readTVar allTasks
+            case M.lookup tid tasks of
+                Nothing -> return ()
+                Just _ -> do
+                    let updatedTasks = M.delete tid tasks
+                    (writeTVar allTasks updatedTasks)
 
 app :: TVar Task -> Application
 app allTasks request respond = case requestMethod request of
@@ -101,8 +181,18 @@ app allTasks request respond = case requestMethod request of
     "POST" -> do
         body <- strictRequestBody request
         case decode body of
-            Just newTask -> (createTask allTasks newTask) >> (putStrLn $ "Received new task: " ++ show (newTask :: TaskBody)) >> respond (responseLBS status200 [("Content-Type", "text/plain")] "Task created successfully")
+            Just newTask -> (createTask allTasks newTask) >> respond (responseLBS status200 [("Content-Type", "text/plain")] "Task created successfully")
             Nothing -> putStrLn "Invalid request body" >> respond (responseLBS status400 [("Content-Type", "text/plain")] "Invalid request body")
+    -- "PUT" -> do
+    --     body <- strictRequestBody request
+    --     case decode body of
+    --         Just currTask -> (updateTask allTasks currTask) >> respond (responseLBS status200 [("Content-Type", "text/plain")] "Task updated successfully")
+    --         Nothing -> putStrLn "Invalid request body" >> respond (responseLBS status400 [("Content-Type", "text/plain")] "Invalid request body")
+    "DELETE" -> do
+        let queries = queryString request
+        let tid = parseQueryItem (L.head queries)
+        deleteTask allTasks tid
+        respond (responseLBS status200 [("Content-Type", "text/plain")] "Task deleted successfully")
     _ -> do
         respond (responseLBS status405 [("Content-Type", "test/plain")] "Method Not Allowed")
 
@@ -113,3 +203,17 @@ serverMain = do
     allTasks <- newTVarIO initTasks -- Initialize an empty task board
     putStrLn "Starting server on port 8080 (http://localhost:8080). Press Ctrl-C to quit."
     run 8080 (app allTasks)
+
+{-
+GET http://localhost:8080/?id=0 -> get task with id 0
+GET http://localhost:8080/?id=-1 -> get all tasks
+
+POST http://localhost:8080/ -> create a new task
+DELETE http://localhost:8080/?id=0 -> delete task with id 0
+
+
+TODO
+PUT http://localhost:8080/?id=0 -> update task with id 0
+
+
+-}
